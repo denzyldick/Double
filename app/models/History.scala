cc2ccd9d51b77e42f3308f02c7cf77bfd2274f0d
Denzyl Dick <denzyl@live.nl>
2015-09-23 21:43:03 +0200

    Learning stuff

---
+++ app/models/History.scala
@@ -0,0 +1,14 @@
+package models
+
+/**
+ * @author denzyl
+ */
+class History(user: User) {
+
+      def get(start:Int,limit:Int): Unit =
+      {
+        /**
+         * Get chat history
+         */
+      }
+}
